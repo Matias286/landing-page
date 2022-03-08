787f646740dccebe088bd8621ae993f9ada30a61
Matias286 <86012502+Matias286@users.noreply.github.com>
2022-03-07 22:24:15 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,3 +3,15 @@ var wpp = "098452420"
 function contacto(){
     alert("mi número telefónico es: "+wpp);
 }
+
+var ubicacion = window.scrollY;
+
+window.onscroll = function(){
+    let desplazamientoActual = window.scrollY;
+    if(ubicacion>desplazamientoActual){
+        document.getElementsByClassName('navegacion-principal').style.top = '0';
+    }else{
+        document.getElementsByClassName('navegacion-principal').style.top = '-100px';
+    }
+    ubicacion = desplazamientoActual;
+}
